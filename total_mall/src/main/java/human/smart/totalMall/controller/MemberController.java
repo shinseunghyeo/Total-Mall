bbe7dc3ea2ec6af6147e817a9a5718c5337c1564
admin <admin@DESKTOP-1KHJHSA>
2024-02-13 10:42:06 +0900

    로그인시 쿠키 추가중

--- total_mall/src/main/java/human/smart/totalMall/controller/MemberController.java
+++ total_mall/src/main/java/human/smart/totalMall/controller/MemberController.java
@@ -1,6 +1,8 @@
 package human.smart.totalMall.controller;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,8 +34,9 @@ public class MemberController {
 	}
 	//로그인 처리 요청
 	@PostMapping("/loginProcess.do")
-	public String loginProcess(String member_id, String member_pw,
-								HttpServletRequest request, Model model) {
+	public String loginProcess(String member_id, String member_pw, 
+								HttpServletRequest request,HttpServletResponse response, Model model,
+								boolean rememberId) {
 		String viewPage = "member/login";
 		
 		MemberVO memberVO = mLogin.login(member_id, member_pw);
@@ -42,8 +45,19 @@ public class MemberController {
 			HttpSession session = request.getSession();
 			session.setAttribute("member", memberVO);
 			viewPage = "redirect:/TotalMall.do";
+			
+			
 		}else {
 			model.addAttribute("msg", "아이디 또는 비밀번호가 맞지 않습니다.");
+		}
+		if(rememberId) {
+			Cookie cookie = new Cookie("member_id", member_id);
+			response.addCookie(cookie);
+			System.out.println(cookie);
+		} else {
+			Cookie cookie = new Cookie("member_id", member_id);
+			cookie.setMaxAge(0);
+			response.addCookie(cookie);
 		}
 			
 		
