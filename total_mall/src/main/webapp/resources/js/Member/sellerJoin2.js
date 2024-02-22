8057abb1867a77c018e1c7e5460912b7e4542840
admin <admin@DESKTOP-1KHJHSA>
2024-02-22 14:47:16 +0900

    css 수정

--- total_mall/src/main/webapp/resources/js/Member/sellerJoin2.js
+++ total_mall/src/main/webapp/resources/js/Member/sellerJoin2.js
@@ -4,42 +4,42 @@ $(function () {
 	/* 유효성 검사 */
     function checkRnameValidity() {
         if (frm_join.representative_name.value.length == 0) {
-            $("#representative_name_test").css("color", "red")
+            $("#representative_name_test").css("color", "black")
         } else {
             $("#representative_name_test").css("color", "white")
         }
     }
     function checkCnumberValidity(){
         if(frm_join.company_number.value.length == 0){
-            $("#company_number_test").css("color","red")
+            $("#company_number_test").css("color","black")
         } else{
             $("#company_number_test").css("color","white")
         }
     }
     function checkMutualValidity(){
         if(frm_join.mutual.value.length == 0){
-            $("#mutual_test").css("color","red")
+            $("#mutual_test").css("color","black")
         } else{
             $("#mutual_test").css("color","white")
         }
     }
     function checkMailOrderValidity(){
         if(frm_join.mail_order_number.value.length == 0){
-            $("#mail_order_number_test").css("color","red")
+            $("#mail_order_number_test").css("color","black")
         } else{
             $("#mail_order_number_test").css("color","white")
         }
     }
     function checkAccountValidity(){
         if(frm_join.account1.value.length == 0){
-            $("#account_test").css("color","red")
+            $("#account_test").css("color","black")
         } else{
             $("#account_test").css("color","white")
         }
     }
     function checkDepositorNameValidity(){
         if(frm_join.depositor_name.value.length == 0){
-            $("#depositor_name_test").css("color","red")
+            $("#depositor_name_test").css("color","black")
         } else{
             $("#depositor_name_test").css("color","white")
         }
@@ -76,12 +76,15 @@ $(function () {
 
         // 전체 유효성 검사 수행
         if (!regExp_id.test(idValue)) {
-            alert("????");
             e.preventDefault(); // 유효성 검사 실패 시 폼 제출 방지
-            idMessage.css("color", "red").text("아이디를 올바르게 입력해주세요.");
+            idMessage.css("color", "black").text("아이디를 올바르게 입력해주세요.");
         }
     });
     /* 유효성 검사 */
+    
+    window.historyBack = function () {
+        window.history.back();
+    };
 
 });
 
