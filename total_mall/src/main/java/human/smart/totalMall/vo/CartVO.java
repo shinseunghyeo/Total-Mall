f2416787ba5856fae53c5080314db6a24ebf7430
admin <admin@DESKTOP-1KHJHSA>
2024-02-26 12:38:26 +0900

    구매 이후 장바구니에 o_idx값 추가해주기

--- total_mall/src/main/java/human/smart/totalMall/vo/CartVO.java
+++ total_mall/src/main/java/human/smart/totalMall/vo/CartVO.java
@@ -12,6 +12,7 @@ public class CartVO {
 	private int c_idx;
 	private int p_idx;
 	private int m_idx;
+	private int o_idx;
 	private int c_quantity;
 	private Date c_start_date;
 	private Date c_update_time;
